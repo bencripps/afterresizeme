c3f148274f4a2f5172dda2fc431fef09d4f8239d
ben_cripps <ben@benjamincripps.com>
2015-08-30 16:38:13 -0500

    Ready for Release

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@
 * @Author: ben_cripps
 * @Date:   2015-08-29 13:12:36
 * @Last Modified by:   ben_cripps
-* @Last Modified time: 2015-08-30 15:44:33
+* @Last Modified time: 2015-08-30 15:52:39
 */
 
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     dirs = {
         src: '/src/*.js',
-        dist: '/dist/'
+        dist: '/dist/afterresize.min.js'
     };
 
 var tasks = {
