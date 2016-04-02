23f5d287276cfe5c1af57ab7c70fa4eea605a625
ben_cripps <ben@benjamincripps.com>
2016-04-01 21:49:49 -0500

    update test utils, add native event test

--- test/src/index.test.js
+++ test/src/index.test.js
@@ -2,24 +2,34 @@ describe('After Resize Library', function() {
 
     var DEFAULT_TIMEOUT = 250;
 
-    var dispatchEvent = function() {
-        window.dispatchEvent(new Event('resize'));
+    var dispatchEvent = function(type) {
+        window.dispatchEvent(new Event(type));
     };
 
-    var addEvent = function(callback, delay) {
+    var addEvent = function(type, callback, delay) {
         window.addEventListener('afterresize', callback, {delay: delay});
     };
 
-    it('Should not degrade native events', function() {
+    it('Should not degrade the native event object', function() {
         var callback = sinon.spy();
-        addEvent(callback, 200);
+        addEvent('afterresize', callback, 200);
 
         expect(document.addEventListener).toBeDefined();
     });
 
+    it('Should allow other events to fire as normal', function() {
+        var callback = sinon.spy();
+        addEvent('click', callback, 200);
+        dispatchEvent('click');
+
+        setTimeout(function() {
+            expect(callback.called).toBe(true);
+        }, 200);
+    });
+
     it('Should not fire if no event has occurred', function() {
         var callback = sinon.spy();
-        addEvent(callback, 200);
+        addEvent('afterresize', callback, 200);
         expect(callback.called).toBe(false);
     });
 
@@ -27,27 +37,27 @@ describe('After Resize Library', function() {
     it('Should fire an event if the event is triggered after the correct delay', function(){
         var DELAY = 200;
         var callback = sinon.spy();
-        addEvent(callback, DELAY);
+        addEvent('afterresize', callback, DELAY);
 
-        dispatchEvent();
+        dispatchEvent('resize');
 
         expect(callback.called).toBe(false);
 
         setTimeout(function() {
             expect(callback.called).toBe(true);
-        }, DELAY)
+        }, DELAY);
     });
 
     it('Should fire an event if no timeout is provided', function(){
         var callback = sinon.spy();
-        addEvent(callback, null);
-        dispatchEvent();
+        addEvent('afterresize', callback, null);
+        dispatchEvent('resize');
 
         expect(callback.called).toBe(false);
 
         setTimeout(function() {
             expect(callback.called).toBe(true);
-        }, DEFAULT_TIMEOUT)
+        }, DEFAULT_TIMEOUT);
     });
 
     it('Should fire number of times its called, not more, not less', function(){
@@ -56,17 +66,17 @@ describe('After Resize Library', function() {
             count++;
         };
 
-        addEvent(callback, 1);
+        addEvent('afterresize', callback, 1);
 
         for (var i = 0; i < 10; i++) {
-            dispatchEvent();
+            dispatchEvent('resize');
         }
 
         expect(count).toEqual(0);
 
         setTimeout(function() {
              expect(count).toEqual(1);
-        }, DEFAULT_TIMEOUT)
+        }, DEFAULT_TIMEOUT);
     });
 
 });
