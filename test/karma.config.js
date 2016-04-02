9f59299b54840de1229f23b755b796d1f2417a72
ben_cripps <ben@benjamincripps.com>
2016-04-01 22:00:24 -0500

    update test config

--- test/karma.config.js
+++ test/karma.config.js
@@ -2,8 +2,16 @@ module.exports = function(config) {
   config.set({
     basePath: '../.',
     frameworks: ['jasmine', 'sinon'],
+    browsers: ['PhantomJS'],
+    plugins: [
+        'karma-phantomjs-launcher',
+        'phantomjs-prebuilt',
+        'karma-jasmine',
+        'karma-sinon'
+    ],
     files: [
         'src', 'test/src/*.test.js'
-    ]
+    ],
+    singleRun: true
   });
 };
